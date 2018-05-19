03967eac6f31b327c2a7d6d42affb076a39a77cb
Albert Podraza <albert.podraza@ge.com>
2018-05-19 18:50:18 +0200

    Heroku default port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path")
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'www')));
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 8080);
 app.listen(app.get("port"), function() {
     console.log("listening to Port", app.get("port"));
 });
